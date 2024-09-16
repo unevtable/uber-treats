80660457d5545c178064182e27174543dd03dc21
unevtable <mahisanchit7@gmail.com>
2024-09-16 16:00:56 +0530

    main

--- index.js
+++ index.js
@@ -3,17 +3,19 @@ import { menuArr } from './data.js'
 const menuEl = document.getElementById('menu')
 
 function renderHTML() {
-   const menuHTML = `
-      <div class="menu-item-container">
-         <img src="/images/pizza.jpg" class="item-img">
-         <div class="item-info">
-            <div class="item-name">Pizza</div>
-            <div class="item-ingredients">pepperoni, mushrom, mozarella</div>
-            <div class="item-price">$14</div>
+   const menuHTML = menuArr.map(function (arr) {
+      return `
+         <div class="menu-item-container">
+            <img src="${arr.image}" class="item-img">
+            <div class="item-info">
+               <div class="item-name">${arr.name}</div>
+               <div class="item-ingredients">${arr.ingredients.join(', ')}</div>
+               <div class="item-price">$${arr.price}</div>
+            </div>
+            <button class="add-button">+</button>
          </div>
-         <button class="add-button">+</button>
-      </div>
    `
+   }).join('')
 
    menuEl.innerHTML += menuHTML
 }
