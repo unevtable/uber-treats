cdcc9315ac719a4ad0cdcae687088002104560cd
unevtable <mahisanchit7@gmail.com>
2024-09-16 21:24:06 +0530

    Image Fix

--- data.js
+++ data.js
@@ -3,21 +3,21 @@ export const menuArr = [
         name: "Pizza",
         ingredients: ["pepperoni", "mushrom", "mozarella"],
         price: 14,
-        image: 'images/pizza.jpg',
+        image: '/images/pizza.jpg',
         id: 0
     },
     {
         name: "Hamburger",
         ingredients: ["beef", "cheese", "lettuce"],
         price: 12,
-        image: 'images/hamburger.jpg',
+        image: '/images/hamburger.jpg',
         id: 1
     },
     {
         name: "Beer",
         ingredients: ["grain, hops, yeast, water"],
         price: 9,
-        image: 'images/beer.jpg',
+        image: '/images/beer.jpg',
         id: 2
     }
 ]
